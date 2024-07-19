3439b1fb5e94b379a3ad3691128d93184b6f179e
Bjorn Ryning <bjorn.ryning@gmail.com>
2024-07-18 20:01:42 -0700

    Initial Commit

---
+++ be bored/be_boredApp.swift
@@ -0,0 +1,17 @@
+//
+//  be_boredApp.swift
+//  be bored
+//
+//  Created by Bjorn Ryning on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct be_boredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
